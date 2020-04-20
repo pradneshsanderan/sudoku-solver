4cdf83a4847b65db114dcbca533ca4583ddccf5f
“[pradnesh <[pradneshsanderan@gmail.com]>
2020-04-20 16:45:17 +0100

    Add existing file

--- src/SudokuUtils.java
+++ src/SudokuUtils.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static java.util.Arrays.fill;
 
@@ -10,6 +11,20 @@ public class SudokuUtils {
         fill(BoardPieces, ".");
         boardPieces = BoardPieces;
     }
+    public static void addPieces(){
+        System.out.println("PLEASE ENTER THE SUDOKU PUZZLE INTO THE DIAGRAM");
+        System.out.println("ENTER THE ROW FOLLOWED BY THE COLLUMN FOLLOWED BY THE NUMBER");
+        Scanner n = new Scanner(System.in);
+        int row = n.nextInt();
+        int column =n.nextInt();
+        int number = n.nextInt();
+        System.out.println("row" + row);
+        System.out.println("col" + column);
+        System.out.println("num" +number);
+        System.out.println(n);
+    }
+
+    
 
 
 
