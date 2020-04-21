77e54cf2f34b2d4ba5be3ced658958d47dddc5bc
“[pradnesh <[pradneshsanderan@gmail.com]>
2020-04-21 09:27:54 +0100

    updated main

--- src/SudokuUtils.java
+++ src/SudokuUtils.java
@@ -5,27 +5,37 @@ import static java.util.Arrays.fill;
 
 public class SudokuUtils {
     public static int dim =9;
+    public static String[] boardPieces = new String[81];
 
-    public static String[] initialiseBoardPieces(){
-        String[] BoardPieces = new String[81];
+    public static String[] initialiseBoardPieces(String[] BoardPieces){
         fill(BoardPieces, ".");
         return BoardPieces;
     }
     public static void addPieces(String[] BoardPieces){
+        boolean valid = false;
         System.out.println("PLEASE ENTER THE SUDOKU PUZZLE INTO THE DIAGRAM");
         System.out.println("ENTER THE ROW FOLLOWED BY THE COLLUMN FOLLOWED BY THE NUMBER");
         Scanner n = new Scanner(System.in);
         int row = n.nextInt();
         int column =n.nextInt();
         int number = n.nextInt();
+        boolean validInput = row<10 & row>0 & column<10 & column>0 & number<10 & number>0;
+       if(!valid){
+           while (!valid){
+           }
+
+       }
+
+
+
+
         int position = (dim*(row-1))+(column-1);
         BoardPieces[position] =String.valueOf(number);
-        System.out.println(BoardPieces[position]);
 
     }
 
     public static void main(String[] args) {
-        addPieces(initialiseBoardPieces());
+        addPieces(initialiseBoardPieces(boardPieces));
     }
 
     
--- src/main.java
+++ src/main.java
@@ -1,8 +1,50 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
         //start game
+        System.out.println("WELCOME TO SUDOKU SOLVER");
+        boolean chosen = false;
+        int choice = 0;
+        while (!chosen){
+            System.out.println("PLEASE CHOOSE ONE OF THE FOLLOWING" +
+                    "\n(1) ADD A SUDOKU AND HAVE IT SOLVED" +
+                    "\n(2) EXIT");
+            Scanner n = new Scanner(System.in);
+            if(n.hasNextInt()){
+                choice = n.nextInt();
+                chosen = true;
+            }
+        }
+        switch (choice) {
+            case 1:
+                SudokuUtils.addPieces(SudokuUtils.initialiseBoardPieces(SudokuUtils.boardPieces));
+                boolean done = false;
+                while (!done){
+                    SudokuUtils.addPieces(SudokuUtils.boardPieces);
+                    Scanner n = new Scanner(System.in);
+                    System.out.println("ARE YOU DONE?" +
+                            "\n INPUT 'Y' FOR YES AND 'N' FOR NO");
+                    if(n.next().equals("Y")){
+                        done = true;
+                    }
+                }
+                for(int i=0;i<SudokuUtils.boardPieces.length;i++){
+                    System.out.println(SudokuUtils.boardPieces[i]);
+                }
+                break;
+            case 0:
 
-        SudokuUI.displayBoard(SudokuUtils.initialiseBoardPieces());
+                break;
+            default:
+
+                break;
+        }
+
+
+
+
+
         //get an empty board
         //input the numbers
         // choose a button when done
